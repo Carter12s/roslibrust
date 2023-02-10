f5e99324f9b0b5635184419b9c52a591c7c09c09
ssnover <ssnover95@gmail.com>
2023-02-09 19:53:46 -0700

    Fix the tests

--- roslibrust_test/src/main.rs
+++ roslibrust_test/src/main.rs
@@ -25,11 +25,13 @@ lazy_static! {
 /// This main function is used to generate the contents of ros1.rs, ros2.rs
 fn main() {
     env_logger::init();
-    let tokens = roslibrust_codegen::find_and_generate_ros_messages((*ROS_1_PATHS).clone());
+    let tokens = roslibrust_codegen::find_and_generate_ros_messages_without_ros_package_path(
+        (*ROS_1_PATHS).clone(),
+    );
     let source = format_rust_source(tokens.to_string().as_str()).to_string();
     let _ = std::fs::write(concat!(env!("CARGO_MANIFEST_DIR"), "/src/ros1.rs"), source);
 
-    let tokens = roslibrust_codegen::find_and_generate_ros_messages(vec![
+    let tokens = roslibrust_codegen::find_and_generate_ros_messages_without_ros_package_path(vec![
         ROS_2_PATH.into(),
         ROS_2_TEST_PATH.into(),
     ]);
