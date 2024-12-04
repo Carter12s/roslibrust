db0b43d21d236e30e1e35812ca4d5a4f76c5226a
carter <carter@amprobotics.com>
2024-12-04 09:25:25 -0700

    Cleanups

--- roslibrust_zenoh/examples/service_call.rs
+++ roslibrust_zenoh/examples/service_call.rs
@@ -8,22 +8,32 @@ use roslibrust::topic_provider::Service;
 // [ServiceProvider] is what allows us to actually access .service_client()
 use roslibrust::topic_provider::ServiceProvider;
 
+// Bring our client type into scope
 use roslibrust_zenoh::ZenohClient;
 
+// Generate rust definitions for our messages compatible with roslibrust
 roslibrust_codegen_macro::find_and_generate_ros_messages!("assets/ros1_common_interfaces");
 
 #[tokio::main]
 async fn main() {
+    // Initialize a logger for debug information while running the example.
+    // Setting the RUST_LOG environment variable can be used to get debug
+    // information if you have issues with this example.
     env_logger::init();
 
+    // Start a zenoh session, depending on how you are running zenoh / the bridge you may
+    // need to pass specific configuration into zenoh::open().
     let session = zenoh::open(zenoh::Config::default()).await.unwrap();
+    // Wrap the zenoh session into a roslibrust client
     let client = ZenohClient::new(session);
 
+    // Create a service client we'll use to call the service
     let client = client
         .service_client::<std_srvs::SetBool>("/my_set_bool")
         .await
         .unwrap();
 
+    // Actually makes the call, sending in the request data, and getting back the response
     let response = client
         .call(&std_srvs::SetBoolRequest { data: true })
         .await
